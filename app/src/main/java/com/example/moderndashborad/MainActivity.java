d6bcafe036a2da1fe3d31f914fe218f509b4ca64
john kaizer <johnkaiser964@gmail.com>
2021-09-08 11:15:48 +0300

    Multiple screens

--- app/src/main/java/com/example/moderndashborad/MainActivity.java
+++ app/src/main/java/com/example/moderndashborad/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.moderndashborad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+  private View icon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        icon= findViewById(R.id.icon);
+        icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,Example.class);
+                startActivity(intent);
+            }
+        });
     }
 }
