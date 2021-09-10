6c14eca9a00f4cf46fdc0fd08df7f2b35e3fa1e4
john kaizer <johnkaiser964@gmail.com>
2021-09-10 11:44:53 +0300

    Bottom navigation

--- app/src/main/java/com/example/moderndashborad/MainActivity.java
+++ app/src/main/java/com/example/moderndashborad/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.moderndashborad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.graphics.drawable.Icon;
@@ -8,19 +9,19 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-  private View icon;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+  private CardView cardTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        icon= findViewById(R.id.icon);
-        icon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this,Example.class);
-                startActivity(intent);
-            }
-        });
+        cardTest=(CardView) findViewById(R.id.cardTest);
+        cardTest.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this , Example.class));
+        finish();
     }
 }
